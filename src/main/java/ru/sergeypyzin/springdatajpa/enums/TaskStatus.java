98f93001778480c4b8ebb1c6f26960f0dbbbc17b
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-04-16 15:17:37 +0300

    create enum TaskStatus

---
+++ src/main/java/ru/sergeypyzin/springdatajpa/enums/TaskStatus.java
@@ -0,0 +1,9 @@
+package ru.sergeypyzin.springdatajpa.enums;
+
+
+/**
+ * Перечисления, представляющие возможные статусы задач
+ */
+public enum TaskStatus {
+    NOT_STARTED, IN_PROGRESS, COMPLETED
+}
