c4adf715f3b0375b3a20f7426c149be5292d84bc
Sergej Pyzin <sergejpyzin@yandex.ru>
2024-04-16 15:15:21 +0300

    create model Task

---
+++ src/main/java/ru/sergeypyzin/springdatajpa/model/Task.java
@@ -0,0 +1,31 @@
+package ru.sergeypyzin.springdatajpa.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import ru.sergeypyzin.springdatajpa.TaskStatus;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+
+
+/**
+ * Класс, представляющий модель задачи.
+ */
+@Data
+@Entity
+@Table (name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false, name = "description")
+    private String description;
+
+    @Column(nullable = false, name = "task_status")
+    private TaskStatus taskStatus = TaskStatus.NOT_STARTED;
+
+    @Column(nullable = false, name = "creation_time")
+    private LocalDateTime creationTime = LocalDateTime.now(Clock.systemDefaultZone());
+}
